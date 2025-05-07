acbcd1125302609c81b211db5f822822db7ff338
nikzn <nikznn46@gmail.com>
2025-05-07 11:39:22 +0530

    create procfile

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from datetime import date
+
+import dotenv
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
@@ -10,6 +12,8 @@ from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+import os
+from dotenv import load_dotenv
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
 
@@ -29,7 +33,7 @@ This will install the packages from the requirements.txt for this project.
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv("SECRETKEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -56,7 +60,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -299,4 +303,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=False, port=5001)
